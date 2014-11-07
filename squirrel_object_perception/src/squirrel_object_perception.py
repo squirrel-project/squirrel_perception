2d4d9080260dc20d969ee924f8c33ec620a5db8e
Markus Bajones <markus.bajones@gmail.com>
2014-11-07 19:35:09 +0100

    add calls to the attention pipeline

--- squirrel_object_perception/src/squirrel_object_perception.py
+++ squirrel_object_perception/src/squirrel_object_perception.py
@@ -3,28 +3,29 @@
 import rospy
 import actionlib
 import squirrel_object_perception_msgs.msg
-from squirrel_object_perception_msgs.srv import ObjectRecognizer
+from squirrel_object_perception_msgs.srv import ObjectRecognizer, \
+    segment_start, segment_once, get_saliency
 from sensor_msgs.msg import PointCloud2
+from std_msgs.msg import Empty
 
 
-class LookForObjectAction(object):
+class LookForObjectsAction(object):
     # create messages that are used to publish feedback/result
-    _feedback = squirrel_object_perception_msgs.msg.LookForObjectFeedback()
-    _result = squirrel_object_perception_msgs.msg.LookForObjectResult()
+    _feedback = squirrel_object_perception_msgs.msg.LookForObjectsFeedback()
+    _result = squirrel_object_perception_msgs.msg.LookForObjectsResult()
     _point_cloud = None
     _objects = None
+    _saliency_map = None
 
     def __init__(self, name):
         self._action_name = name
         self._as = actionlib.SimpleActionServer(
             self._action_name,
-            squirrel_object_perception_msgs.msg.LookForObjectAction,
+            squirrel_object_perception_msgs.msg.LookForObjectsAction,
             execute_cb=self.execute_cb,
             auto_start=False
         )
         self._as.start()
-        self.recognizer = rospy.ServiceProxy(
-            'mp_recognition', ObjectRecognizer)
 
     def set_publish_feedback(self, phase, status, percent):
         self._feedback.current_phase = phase
@@ -32,6 +33,58 @@ class LookForObjectAction(object):
         self._feedback.percent_completed = percent
         self._as.publish_feedback(self._feedback)
         return
+
+    def do_recognition(self):
+        recognizer = rospy.ServiceProxy('mp_recognition', ObjectRecognizer)
+        result = None
+        self.set_publish_feedback('recognition', 'started', 11)
+        try:
+            rospy.wait_for_service('mp_recognition', timeout=5)
+            result = recognizer(self._point_cloud)
+            self.set_publish_feedback('recognition', 'done', 50)
+        except rospy.ROSException as e:
+            self.set_publish_feedback('recognition', 'service call failed', 11)
+            rospy.logdebug('mp_recognition: %s' % str(e))
+        return result
+
+    def get_saliency_map(self):
+        get_saliency_map = rospy.ServiceProxy(
+            'squirrel_attention_location', get_saliency)
+        saliency_map = None
+        try:
+            rospy.wait_for_service('squirrel_attention_location', timeout=5)
+            self._saliency_map = get_saliency_map(self._point_cloud)
+            self.set_publish_feedback('attention', 'done', 50)
+        except rospy.ROSException as e:
+            self.set_publish_feedback('attention', 'service call failed', 11)
+            rospy.logdebug('attention: %s' % str(e))
+        return saliency_map
+
+    def setup_segmentation(self):
+        init_segmenter = rospy.ServiceProxy(
+            'squirrel_segmentation_incremental_start', segment_start)
+        try:
+            rospy.wait_for_service(
+                'squirrel_segmentation_incremental_start', timeout=5)
+            init_segmenter(self._point_cloud, self.saliency_map)
+            self.set_publish_feedback('attention', 'done', 50)
+            return True
+        except rospy.ROSException as e:
+            self.set_publish_feedback('attention', 'service call failed', 11)
+            rospy.logdebug('attention: %s' % str(e))
+            return False
+
+    def run_segmenter_once(self):
+        do_segment = rospy.ServiceProxy(
+            'squirrel_segmentation_incremental_once', segment_once)
+        try:
+            rospy.wait_for_service(
+                'squirrel_segmentation_incremental_once', timeout=5)
+            self.segment_result = do_segment(Empty)
+            self.set_publish_feedback('attention', 'done', 50)
+        except rospy.ROSException as e:
+            self.set_publish_feedback('attention', 'service call failed', 11)
+            rospy.logdebug('attention: %s' % str(e))
 
     def execute_cb(self, goal):
         # initialize feedback
@@ -57,22 +110,17 @@ class LookForObjectAction(object):
             rospy.loginfo('Aborted' % self._action_name)
             self._as.set_aborted(self._result)
             return
-
-        # publish the feedback
         self.set_publish_feedback('receive_data', 'done', 10)
-        self.set_publish_feedback('recognition', 'started', 11)
-        # call Aitor's recognition service
-        try:
-            rospy.wait_for_service('mp_recognition', timeout=5)
-            self.set_publish_feedback('recognition', 'done', 50)
-        except rospy.ROSException as e:
-            self.set_publish_feedback('recognition', 'service call failed', 11)
-            rospy.logdebug('mp_recognition: %s' % str(e))
 
+        # call Aitor's recognition service
+        self.do_recognition()
         # Start attention, segmentation classification pipeline
         self.set_publish_feedback('attention', 'started', 51)
+        self.get_saliency_map()
         self.set_publish_feedback('attention', 'done', 60)
         self.set_publish_feedback('segmentation', 'started', 61)
+        self.setup_segmentation()
+        self.run_segmenter_once()
         self.set_publish_feedback('segmentation', 'done', 75)
         self.set_publish_feedback('classification', 'started', 76)
         self.set_publish_feedback('classification', 'done', 97)
@@ -96,5 +144,5 @@ class LookForObjectAction(object):
 
 if __name__ == '__main__':
     rospy.init_node('squirrel_object_perception')
-    LookForObjectAction(rospy.get_name())
+    LookForObjectsAction(rospy.get_name())
     rospy.spin()
