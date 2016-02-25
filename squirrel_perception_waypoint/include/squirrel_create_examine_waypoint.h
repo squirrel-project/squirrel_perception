48ad1527f31cdfbb6135329dd8c386e666ef52c8
Edith Langer <langer@acin.tuwien.ac.at>
2016-02-25 16:34:15 +0100

    create examining waypoints depending on object diameter

--- squirrel_perception_waypoint/include/squirrel_create_examine_waypoint.h
+++ squirrel_perception_waypoint/include/squirrel_create_examine_waypoint.h
@@ -15,6 +15,7 @@
 #include "squirrel_waypoint_msgs/ExamineWaypoint.h"
 #include "squirrel_waypoint_msgs/ExamineWaypointRequest.h"
 #include "squirrel_waypoint_msgs/ExamineWaypointResponse.h"
+#include "squirrel_object_perception_msgs/BCylinder.h"
 
 class CreateExamineWaypoint {
 
@@ -35,7 +36,7 @@ public:
     void initialize (int argc, char ** argv);
     bool createExamineWaypoints(squirrel_waypoint_msgs::ExamineWaypointRequest &req, squirrel_waypoint_msgs::ExamineWaypointResponse &resp);
 
-    void visualizeWayPoints(geometry_msgs::PoseStamped lump_pose, squirrel_waypoint_msgs::ExamineWaypointResponse &resp);
+    void visualizeWayPoints(squirrel_waypoint_msgs::ExamineWaypointRequest &req, squirrel_waypoint_msgs::ExamineWaypointResponse &resp);
 };
 
 #endif
