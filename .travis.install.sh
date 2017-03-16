4388acd8e32282eb1aed14641884ea9fd6416171
ipa-nhg <nhg@ipa.fhg.de>
2017-03-16 14:49:40 +0100

    fix travis config

--- .travis.install.sh
+++ .travis.install.sh
@@ -2,5 +2,14 @@ set -e
 set -v
 
 echo "BEFORE INSTALL IS RUNNING"
-sudo sh -c 'echo "deb http://153.97.4.193/building/ubuntu trusty main" > /etc/apt/sources.list'
-wget http://153.97.4.193/building/public.key -O - | sudo apt-key add -
+sh -c 'echo "deb http://153.97.4.193/building/ubuntu trusty main" > /etc/apt/sources.list'
+wget http://153.97.4.193/building/public.key -O - | apt-key add -
+
+sh -c 'echo "deb http://lcas.lincoln.ac.uk/repos/release trusty main" > /etc/apt/sources.list'
+wget http://lcas.lincoln.ac.uk/repos/public.key -O - | apt-key add -
+apt-get update
+
+mkdir -p /etc/ros/rosdep/mapping/
+cp squirrel_perception/rosdep/private.yaml /etc/ros/rosdep/mapping/private_perception.yaml
+cp squirrel_perception/rosdep/31-squirrel_perception.list /etc/ros/rosdep/sources.list.d/31-squirrel_perception.list
+rosdep update
