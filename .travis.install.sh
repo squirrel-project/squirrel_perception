5f827b8c5113f1625ecbfdedefd248b72174d911
ipa-nhg <nhg@ipa.fhg.de>
2017-03-17 16:02:57 +0100

    fix travis configuration

--- .travis.install.sh
+++ .travis.install.sh
@@ -1,46 +1,17 @@
 set -e
 set -v
 
-while true; do echo "INSTALL IS RUNNING" && sleep 60; done&
+echo "BEFORE INSTALL IS RUNNING"
+sh -c 'echo "deb http://153.97.4.193/building/ubuntu trusty main" > /etc/apt/sources.list.d/squirrel_server.list'
+wget http://153.97.4.193/building/public.key -O - | apt-key add -
+apt-get update
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo apt-get update -qq > /dev/null 2>&1
-sudo apt-get install -qq -y python-rosdep python-wstool > /dev/null 2>&1
-sudo apt-get install -qq -y ros-${CI_ROS_DISTRO}-ros > /dev/null 2>&1 #needed as long as https://github.com/ros-infrastructure/rosdep/issues/430 is not fixed
-sudo rosdep init
+sh -c 'echo "deb http://lcas.lincoln.ac.uk/repos/release trusty main" > /etc/apt/sources.list.d/lincoln_server.list'
+wget http://lcas.lincoln.ac.uk/repos/public.key -O - | apt-key add -
+apt-get update
+
+mkdir -p /etc/ros/rosdep/mapping/
+cp private.yaml /etc/ros/rosdep/mapping/private_perception.yaml
+cp 31-squirrel_perception.list /etc/ros/rosdep/sources.list.d/31-squirrel_perception.list
 rosdep update
 
-## SQUIRREL SPECIFIC ##
-sudo sh -c 'echo "deb http://153.97.4.193/building/ubuntu trusty main" > /etc/apt/sources.list'
-wget http://153.97.4.193/building/public.key -O - | sudo apt-key add -
-
-sudo sh -c 'echo "deb http://lcas.lincoln.ac.uk/repos/release trusty main" > /etc/apt/sources.list'
-wget http://lcas.lincoln.ac.uk/repos/public.key -O - | sudo apt-key add -
-sudo apt-get update
-
-sudo mkdir -p /etc/ros/rosdep/mapping/
-sudo cp squirrel_perception/rosdep/private.yaml /etc/ros/rosdep/mapping/private_perception.yaml
-sudo cp squirrel_perception/rosdep/31-squirrel_perception.list /etc/ros/rosdep/sources.list.d/31-squirrel_perception.list
-rosdep update
-
-# create empty underlay workspace
-mkdir -p $CATKIN_WS_UNDERLAY_SRC
-source /opt/ros/$CI_ROS_DISTRO/setup.bash > /dev/null 2>&1 # source release
-catkin_init_workspace $CATKIN_WS_UNDERLAY_SRC
-cd $CATKIN_WS_UNDERLAY
-catkin_make -DCMAKE_BUILD_TYPE=Release # build empty underlay devel space
-catkin_make -DCMAKE_BUILD_TYPE=Release install # build empty underlay install space
-# populate underlay
-if [ -f $TRAVIS_BUILD_DIR/.travis.rosinstall ]; then wstool init -j10 src $TRAVIS_BUILD_DIR/.travis.rosinstall; fi
-if [ ! -f $TRAVIS_BUILD_DIR/.travis.rosinstall ]; then wstool init -j10 src $DEFAULT_ROSINSTALL; fi
-# install dependencies from underlay
-rosdep install -q --from-paths $CATKIN_WS_UNDERLAY_SRC -i -y --rosdistro $CI_ROS_DISTRO > /dev/null #2>&1
-# build devel space of underlay
-source $CATKIN_WS_UNDERLAY/devel/setup.bash > /dev/null 2>&1 # source devel space of underlay
-catkin_make -DCMAKE_BUILD_TYPE=Release
-# build install space of underlay
-catkin_make -DCMAKE_BUILD_TYPE=Release install > /dev/null #2>&1
-ret=$?
-kill %%
-exit $ret
