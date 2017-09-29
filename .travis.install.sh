4fb3059e30875b41002c998c05eb131c108fd8cf
Nadia Hammoudeh García <nhg@ipa.fhg.de>
2017-09-29 08:55:02 +0200

    updated v4r deb in 153.97.4.193 server

--- .travis.install.sh
+++ .travis.install.sh
@@ -6,8 +6,8 @@ sh -c 'echo "deb http://lcas.lincoln.ac.uk/repos/release trusty main" > /etc/apt
 wget http://lcas.lincoln.ac.uk/repos/public.key -O - | apt-key add -
 apt-get update
 
-sh -c 'echo "deb http://rwiki.acin.tuwien.ac.at/apt/v4r-release trusty main" > /etc/apt/sources.list.d/rwiki_server.list'
-wget http://rwiki.acin.tuwien.ac.at/apt/Public.key -O - | apt-key add -
+sh -c 'echo "deb http://153.97.4.193/building/ubuntu trusty main" > /etc/apt/sources.list.d/squirrel_server.list'
+wget http://153.97.4.193/building/public.key -O - | apt-key add -
 apt-get update
 
 mkdir -p /etc/ros/rosdep/mapping/
