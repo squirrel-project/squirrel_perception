193904a1daba78f776f361771e185590653a2d31
ipa-nhg <nhg@ipa.fhg.de>
2017-10-16 11:53:01 +0200

    update travis pre install to use the rwiki server

--- .travis.install.sh
+++ .travis.install.sh
@@ -2,8 +2,9 @@ set -e
 set -v
 
 echo "BEFORE INSTALL IS RUNNING"
-sh -c 'echo "deb http://153.97.4.193/building/ubuntu trusty main" > /etc/apt/sources.list.d/squirrel_server.list'
-wget http://153.97.4.193/building/public.key -O - | apt-key add -
+apt-get install apt-transport-https
+sh -c 'echo "deb [arch=amd64] https://rwiki.acin.tuwien.ac.at/apt/v4r-release trusty main" > /etc/apt/sources.list.d/squirrel_server.list'
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-key 943EB54F
 apt-get update
 
 mkdir -p /etc/ros/rosdep/mapping/
