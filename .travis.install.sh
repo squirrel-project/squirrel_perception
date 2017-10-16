9c87cbbf66fbd8831b1531dcb6bef24d06834984
ipa-nhg <nhg@ipa.fhg.de>
2017-10-16 11:56:26 +0200

    update the key

--- .travis.install.sh
+++ .travis.install.sh
@@ -4,7 +4,7 @@ set -v
 echo "BEFORE INSTALL IS RUNNING"
 apt-get install apt-transport-https
 sh -c 'echo "deb [arch=amd64] https://rwiki.acin.tuwien.ac.at/apt/v4r-release trusty main" > /etc/apt/sources.list.d/squirrel_server.list'
-apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-key 943EB54F
+wget -qO - https://rwiki.acin.tuwien.ac.at/apt/v4r-release/Public.key | sudo apt-key add -
 apt-get update
 
 mkdir -p /etc/ros/rosdep/mapping/
