eb03b8fc48b3db7c4be091e8438cb7770988977a
Edith Langer <langer@acin.tuwien.ac.at>
2018-01-23 12:14:32 +0100

    smarter way of creating waypoints around a lump with taking octomap into account

--- squirrel_perception_waypoint/include/squirrel_create_examine_waypoint.h
+++ squirrel_perception_waypoint/include/squirrel_create_examine_waypoint.h
@@ -3,6 +3,7 @@
 
 #include "ros/ros.h"
 #include <sstream>
+#include <vector>
 #include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/PoseStamped.h>
@@ -10,6 +11,8 @@
 #include <visualization_msgs/MarkerArray.h>
 #include <geometry_msgs/PoseArray.h>
 #include <math.h>
+#include <octomap/octomap.h>
+#include <octomap/OcTree.h>
 
 #include "std_srvs/Empty.h"
 #include "squirrel_waypoint_msgs/ExamineWaypoint.h"
@@ -19,8 +22,21 @@
 
 class CreateExamineWaypoint {
 
-    static const double distance_to_lump = 0.40; //in meters
-    static const int nr_of_waypoints = 5;
+    //static const double distance_to_lump = 0.40; // min. distance in meters
+    static const double robot_base_dia = 0.45;   // diameter of robot's base
+    static const double robot_height = 0.8;      // robot's full height
+    static const double camera_height = 0.68;    // camera mounting height
+    //static const int nr_of_waypoints = 5;        /* aimed at number of waypoints around object */
+    static const double hApAngle = 57*M_PI/180;  // horiz. cam aperture angle
+    static const double vApAngle = 43*M_PI/180;  // vert. cam aperture angle
+    static const double maxPanTilt = M_PI/(2*M_PI_2); // angle at pan/tilt = 1
+
+    struct wpData {          // waypoint structure for output (internal only)
+        octomap::point3d pos;  // position of found waypoint
+        float pan;             // max camer pan
+        float tiltLB;          // camera tilt (lower bound)
+        float tiltUB;          //     -"-     (upper bound)
+    };
 
 private:
     ros::ServiceServer examineWaypointsServer;
@@ -28,6 +44,11 @@ private:
 
     ros::Publisher markerPublisher;
     ros::Publisher posePublisher;
+
+    double distance_to_lump;
+    int nr_of_waypoints;
+
+    octomap::OcTree *oct = NULL;
 
 public:
     CreateExamineWaypoint ();
@@ -37,6 +58,11 @@ public:
     bool createExamineWaypoints(squirrel_waypoint_msgs::ExamineWaypointRequest &req, squirrel_waypoint_msgs::ExamineWaypointResponse &resp);
 
     void visualizeWayPoints(squirrel_waypoint_msgs::ExamineWaypointRequest &req, squirrel_waypoint_msgs::ExamineWaypointResponse &resp);
+
+private:
+    bool isBBXOccupied(octomap::OcTree *oct, octomap::point3d min, octomap::point3d max);
+    bool readOctomapFromFile(std::string fileOctomap);
+    int computeWaypoints(octomap::OcTree *oct, geometry_msgs::PoseStamped lump, float radius, float height, std::vector<wpData> &wp);
 };
 
 #endif
